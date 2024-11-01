74c514a9bb701ace1fd5d3e225622e6a40f1181e
Yulia Arkadeva <yulia.arkadeva@plot-a.eu>
2024-11-01 12:51:35 +0100

    save for computer cleaning

---
+++ augmentation.py
@@ -0,0 +1,225 @@
+import os
+import xml.etree.ElementTree as ET
+from PIL import Image, ImageEnhance
+from torchvision import transforms
+import random
+import copy
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import glob
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import cv2
+import numpy as np
+from data_processing import get_person_label
+
+def identify_minor_classes(class_counts, threshold=500):
+    minor_classes = {cls: count for cls, count in class_counts.items() if count < threshold}
+    print("minor_classes:")
+    print(minor_classes)
+    return minor_classes
+
+def identify_domenant_classes(class_counts, threshold=1500):
+   
+    domenant_classes = {cls for cls, count in class_counts.items() if count > threshold}
+    print("domenant_classes:")
+    print(domenant_classes)
+    return domenant_classes
+
+def augment_image(image, boxes):
+    """
+    Применяет аугментацию к изображению и обновляет bounding boxes.
+    
+    Параметры:
+    - image (numpy.ndarray): изображение в формате numpy.
+    - boxes (list): список координат bounding boxes.
+    
+    Возвращает:
+    - augmented_image (numpy.ndarray): аугментированное изображение.
+    - augmented_boxes (list): обновленные координаты bounding boxes.
+    """
+    transform = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.2),
+        A.Rotate(limit=15, p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=0, p=0.5),
+        ToTensorV2()
+    ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=[]))
+
+    transformed = transform(image=image, bboxes=boxes)
+    augmented_image = transformed['image'].permute(1, 2, 0).numpy()
+    augmented_image = (augmented_image * 255).astype(np.uint8)
+    augmented_boxes = transformed['bboxes']
+    
+    return augmented_image, augmented_boxes
+
+def save_augmented_data(augmented_image, original_annotation_path, augmented_boxes, output_image_path, output_annotation_path):
+    """
+    Сохраняет аугментированное изображение и обновленную аннотацию в XML.
+    
+    Параметры:
+    - augmented_image (numpy.ndarray): аугментированное изображение.
+    - original_annotation_path (str): путь к оригинальной аннотации.
+    - augmented_boxes (list): обновленные bounding boxes.
+    - output_image_path (str): путь для сохранения изображения.
+    - output_annotation_path (str): путь для сохранения XML аннотации.
+    """
+    Image.fromarray(augmented_image).save(output_image_path)
+    tree = ET.parse(original_annotation_path)
+    root = tree.getroot()
+    
+    for obj, box in zip(root.findall('object'), augmented_boxes):
+        bndbox = obj.find('bndbox')
+        bndbox.find('xmin').text = str(int(box[0]))
+        bndbox.find('ymin').text = str(int(box[1]))
+        bndbox.find('xmax').text = str(int(box[2]))
+        bndbox.find('ymax').text = str(int(box[3]))
+
+    tree.write(output_annotation_path)
+
+def augment_voc_classes(image_names, images_dir, annotations_dir, classes, output_dir, domenant_classes, target_multiplier=2):
+    """
+    Выполняет аугментацию только для изображений, содержащих объекты меньших классов
+    и не содержащих объекты из доминирующих классов.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    output_images_dir = os.path.join(output_dir, 'Images')
+    output_annotations_dir = os.path.join(output_dir, 'Annotations')
+    os.makedirs(output_images_dir, exist_ok=True)
+    os.makedirs(output_annotations_dir, exist_ok=True)
+
+    for cls, count in classes.items():
+        target_count = count * target_multiplier
+        current_count = 0
+        
+        for image_name in image_names:
+            annotation_file = image_name.replace('.jpg', '.xml')
+            annotation_path = os.path.join(annotations_dir, annotation_file)
+            
+            if not os.path.exists(annotation_path):
+                continue
+
+            # Загружаем изображение и проверяем наличие только целевого класса
+            image, boxes, is_valid = load_image_with_class_check(image_name, images_dir, annotation_path, cls, domenant_classes)
+            if not is_valid:
+                continue  # Пропускаем изображение, если оно содержит доминирующий класс
+            
+            for i in range(target_multiplier):
+                if current_count >= target_count:
+                    break
+                
+                augmented_image, augmented_boxes = augment_image(image, boxes)
+                
+                output_image_path = os.path.join(output_images_dir, f"{cls}_aug_{current_count}.jpg")
+                output_annotation_path = os.path.join(output_annotations_dir, f"{cls}_aug_{current_count}.xml")
+                save_augmented_data(augmented_image, annotation_path, augmented_boxes, output_image_path, output_annotation_path)
+                
+                current_count += 1
+
+def visualize_random_augmented_image(augmented_images_dir, augmented_annotations_dir):
+    """
+    Отображает случайное аугментированное изображение с bounding boxes из аннотации.
+    
+    Параметры:
+    - augmented_images_dir (str): директория с аугментированными изображениями.
+    - augmented_annotations_dir (str): директория с аннотациями.
+    """
+    image_files = glob.glob(os.path.join(augmented_images_dir, '*.jpg'))
+    random_image_path = random.choice(image_files)
+    image_name = os.path.basename(random_image_path)
+    annotation_path = os.path.join(augmented_annotations_dir, image_name.replace('.jpg', '.xml'))
+    
+    if not os.path.exists(annotation_path):
+        print(f"No annotation found for image {random_image_path}")
+        return
+    
+    visualize_augmented_image_with_boxes(random_image_path, annotation_path)
+
+def load_image_with_class_check(image_name, images_dir, annotation_path, target_class, dominant_classes):
+    """
+    Загружает изображение и bounding boxes только для объектов целевого класса, 
+    исключая изображения, содержащие объекты преобладающих классов.
+
+    Параметры:
+    - image_name (str): имя файла изображения.
+    - images_dir (str): директория с изображениями.
+    - annotation_path (str): путь к аннотации (XML файл).
+    - target_class (str): целевой класс для аугментации (может включать подклассы person).
+    - dominant_classes (set): множество преобладающих классов для исключения.
+
+    Возвращает:
+    - image (np.array): изображение.
+    - boxes (list): список bounding boxes для целевого класса.
+    - is_valid (bool): True, если целевой класс найден и преобладающие классы отсутствуют, иначе False.
+    """
+    image_path = os.path.join(images_dir, image_name)
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = []
+    contains_target_class = False
+    contains_dominant_class = False
+    tree = ET.parse(annotation_path)
+    root = tree.getroot()
+
+    for obj in root.findall('object'):
+        label = get_person_label(obj)
+        # label = obj.find('name').text
+
+        # # Проверка для подклассов person
+        # if label == "person":
+        #     pose = obj.find('pose').text if obj.find('pose') is not None else 'Unspecified'
+        #     action = None
+        #     if pose == "Unspecified":  # Используем action, если pose не указана
+        #         actions = obj.find('actions')
+        #         if actions is not None:
+        #             for act in actions:
+        #                 if int(act.text) == 1:
+        #                     action = act.tag
+        #                     break
+        #     label = f"person_{pose.lower()}" if pose != "Unspecified" else f"person_{action.lower() if action else 'unspecified'}"
+        
+        # Проверяем, совпадает ли label с целевым классом
+        if label == target_class:
+            contains_target_class = True
+            bndbox = obj.find('bndbox')
+            xmin = int(float(bndbox.find('xmin').text))
+            ymin = int(float(bndbox.find('ymin').text))
+            xmax = int(float(bndbox.find('xmax').text))
+            ymax = int(float(bndbox.find('ymax').text))
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # Проверка на доминирующий класс
+        if label in dominant_classes:
+            contains_dominant_class = True
+            break
+
+    is_valid = contains_target_class and not contains_dominant_class
+    return image, boxes, is_valid
+
+def visualize_augmented_image_with_boxes(image_path, annotation_path):
+   
+    image = Image.open(image_path)
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    ax.imshow(image)
+
+    tree = ET.parse(annotation_path)
+    root = tree.getroot()
+    
+    for obj in root.findall('object'):
+        bndbox = obj.find('bndbox')
+        xmin = int(bndbox.find('xmin').text)
+        ymin = int(bndbox.find('ymin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymax = int(bndbox.find('ymax').text)
+        
+        width = xmax - xmin
+        height = ymax - ymin
+        rect = patches.Rectangle((xmin, ymin), width, height, linewidth=2, edgecolor='r', facecolor='none')
+        
+        ax.add_patch(rect)
+        label = obj.find('name').text
+        ax.text(xmin, ymin - 10, label, color='red', fontsize=12, weight='bold')
+
+    plt.axis('off')
+    plt.show()
--- main.py
+++ main.py
@@ -1,59 +1,184 @@
+# main.py
 import os
 import random
-import numpy as np
-import pandas as pd
-import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
-import seaborn as sns
-from PIL import Image
+import torch
+from torch.utils.data import DataLoader
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from voc_dataset import VOCDataset  # Импортируем класс VOCDataset
+from data_processing import (visualize_image_and_tensor, validate_labels, 
+                             count_classes, plot_class_distribution, 
+                             filter_person_and_other_images, count_poses, 
+                             plot_pose_distribution)
+from torchvision.transforms import functional as F
+from augmentation import augment_image, identify_minor_classes, identify_domenant_classes, visualize_augmented_image_with_boxes, visualize_random_augmented_image, augment_voc_classes
+from resizing import resize_images_and_boxes
+from weights_selection import create_weighted_sampler
 
+import ssl
+import certifi
+
+ssl._create_default_https_context = ssl._create_unverified_context 
+ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
+
+
+# start links
 data_dir = './dataset/Images'
 annotations_dir = './dataset/Annotations'
-
-#regular values
 train_split = 0.7
 val_split = 0.15
 test_split = 0.15
+num_epochs = 5
 
-#fixing of shuffle order
+# fix random seed for repeating in rnadomizer
 random.seed(42)
 
-#IDEA: when the model will work: detect classes of images and generate annotations
-
-
-# checks if annotation for images exists, othervise image is not labeled and doesnt fit for steps before the real expluatation
-def validate_labels(images, annotations_dir):
-    valid_images = []
-    black_list = []
-    
-    for image_file in images:
-        annotation_file = image_file.replace('.jpg', '.xml')
-        annotation_path = os.path.join(annotations_dir, annotation_file)
-        
-        
-        if os.path.exists(annotation_path):
-            valid_images.append(image_file)
-        else:
-            black_list.append(os.path.join(data_dir, image_file))
-    
-    return valid_images, black_list
-
-#it gets all images list
+# 
 images = [f for f in os.listdir(data_dir) if f.endswith('.jpg')]
+#divide all not annotated images
+valid_images, black_list = validate_labels(images, annotations_dir, data_dir)
+
+print("Black list (no labels):", len(black_list))
+
+resized_images_dir = './resized_dataset/Images'
+resized_annotations_dir = './resized_dataset/Annotations'
+
+resize_images_and_boxes(
+    image_names=valid_images,
+    images_dir=data_dir,
+    annotations_dir=annotations_dir,
+    output_images_dir=resized_images_dir,
+    output_annotations_dir=resized_annotations_dir,
+    target_size=(224, 224)
+)
+
+for index in range(10):
+    visualize_random_augmented_image(resized_images_dir, resized_annotations_dir)
+
+#after resising the resized dirs become new defaut dirs
+data_dir = resized_images_dir
+annotations_dir = resized_annotations_dir
+
+random.shuffle(valid_images)
 
 
-valid_images, black_list = validate_labels(images, annotations_dir)
-
-random.shuffle(valid_images)  #случайное перемешивание
-
+# Разделяем данные
 train_size = int(train_split * len(valid_images))
 val_size = int(val_split * len(valid_images))
-
 train_images = valid_images[:train_size]
 val_images = valid_images[train_size:train_size + val_size]
 test_images = valid_images[train_size + val_size:]
 
 
-print("Black list (no labels):", len(black_list))
-#visualisation of classes distribution
+# person class dividion distribution
+pose_counts = count_poses(annotations_dir, train_images)
+plot_pose_distribution(pose_counts, title="Pose and actions Distribution in Training Dataset")
 
+train_class_counts = count_classes(annotations_dir, train_images)
+plot_class_distribution(train_class_counts, title="Class Distribution in Training Set")
+
+# validation_class_counts = count_classes(annotations_dir, val_images)
+# plot_class_distribution(validation_class_counts, title="Class Distribution in Validation Set")
+
+#detecting classed from train dataset wich need to be augmented
+minor_classes = identify_minor_classes(train_class_counts, threshold=500)
+domenant_classes = identify_domenant_classes(train_class_counts, threshold=1500)
+augment_voc_classes(classes=minor_classes, image_names=train_images, images_dir=data_dir,  annotations_dir=annotations_dir, output_dir='./resized_dataset', target_multiplier=2, domenant_classes=domenant_classes)
+
+# Обновляем train_images, чтобы включить новые аугментированные изображения
+augmented_images = [f for f in os.listdir(data_dir) if '_aug_' in f and f.endswith('.jpg')]
+train_images.extend(augmented_images)  # Добавляем аугментированные изображения в список тренировочных данных
+
+#check distribution one morw time
+train_class_counts = count_classes(annotations_dir, train_images)
+plot_class_distribution(train_class_counts, title="Class Distribution in Training Set after augmentation")
+
+# visalisation
+for index in range(10):
+    visualize_random_augmented_image(data_dir, annotations_dir)
+
+
+
+sampler = create_weighted_sampler(train_images, annotations_dir)
+
+#checked till here
+
+
+# Создаем DataLoader для каждой выборки
+train_dataset = VOCDataset(train_images, annotations_dir, data_dir, transforms=F.to_tensor)
+val_dataset = VOCDataset(val_images, annotations_dir, data_dir, transforms=F.to_tensor)
+test_dataset = VOCDataset(test_images, annotations_dir, data_dir, transforms=F.to_tensor)
+
+train_loader = DataLoader(train_dataset, batch_size=2, sampler=sampler, collate_fn=lambda x: tuple(zip(*x)))
+val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
+test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
+
+unique_classes = VOCDataset.get_unique_classes(annotations_dir, valid_images)
+num_classes = len(unique_classes) + 1  # +1 для фона
+
+print("Unique classes:", unique_classes)
+print("Number of classes (including background):", num_classes)
+
+# Инициализируем модель Faster R-CNN с предобученной ResNet50
+model = fasterrcnn_resnet50_fpn(pretrained=True)
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = torch.nn.Linear(in_features, num_classes)
+
+# Установка устройства
+device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
+model.to(device)
+
+# Оптимизатор
+optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+
+# Функция для оценки на валидационном наборе
+def evaluate(model, data_loader, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():  # Отключаем градиенты для оценки
+        for images, targets in data_loader:
+            images = [image.to(device) for image in images]
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            loss_dict = model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+            total_loss += losses.item()
+    print(f"Validation Loss: {total_loss / len(data_loader)}")
+    model.train()  # Возвращаем модель в режим обучения
+
+# Функция для обучения одной эпохи
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    total_loss = 0
+    for images, targets in data_loader:
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+        
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+        
+        total_loss += losses.item()
+    print(f"Epoch {epoch}, Loss: {total_loss / len(data_loader)}")
+
+model_save_path = "faster_rcnn_model.pth"
+
+# Тренировка модели
+for epoch in range(num_epochs):
+    train_one_epoch(model, optimizer, train_loader, device, epoch)
+    evaluate(model, val_loader, device)  # Оценка на валидационном наборе
+
+    torch.save(model.state_dict(), model_save_path)
+    print(f"Model saved after epoch {epoch + 1} at {model_save_path}")
+
+# Оценка модели на тестовых данных
+model.eval()
+all_predictions = []
+with torch.no_grad():
+    for images, targets in test_loader:
+        images = list(image.to(device) for image in images)
+        predictions = model(images)
+        all_predictions.extend(predictions)
+
+print("Model evaluation completed.")
